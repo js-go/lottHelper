a3e16259f48f8b7fa808f5a2becd0cef9bb1d09d
eep <coolicer@foxmail.com>
2018-04-08 20:49:55 +0800

    增加2个字段

--- src/db/models/lottery.js
+++ src/db/models/lottery.js
@@ -5,10 +5,12 @@ module.exports = function (sequelize, DataTypes) {
     id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
     periods: { type: Sequelize.INTEGER, allowNull: false }, // 彩票期数
     is_signle: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true }, //1：单式  2：复式
-    numbers: {type: Sequelize.STRING, allowNull: false }, // 号码
-    name: {type: Sequelize.STRING, allowNull: false }, // 彩票名称
-    user_id: {type: Sequelize.STRING, allowNull: false },
-    species: {type: Sequelize.STRING, allowNull: false } //1：福彩  2：体彩
+    numbers: { type: Sequelize.STRING, allowNull: false }, // 号码
+    name: { type: Sequelize.STRING, allowNull: false }, // 彩票名称
+    user_id: { type: Sequelize.STRING, allowNull: false },
+    species: { type: Sequelize.STRING, allowNull: false }, //1：福彩  2：体彩,
+    winning: { type: Sequelize.STRING }, // 1为一等奖 ....空为不中奖
+    check: { type: Sequelize.BOOLEAN,  defaultValue: false }
   })
 
   return Lottery
